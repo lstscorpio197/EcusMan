fe2c70bfb2ccd851ac3826f339d5037ddbf90be7
lstscorpio197 <70469361+lstscorpio197@users.noreply.github.com>
2022-10-20 16:33:23 +0700

    no message

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -20,6 +20,7 @@ namespace ECUS_MAN
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/umd/popper.js",
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
